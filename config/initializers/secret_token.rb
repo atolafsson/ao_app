a10d5566042e74b3bc3fdbed4e6a50366e495380
Atli Olafsson <atolafsson@gmail.com>
2014-09-19 22:29:12 -0700

    First in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AoApp::Application.config.secret_key_base = '35a859a6479605b79da072705a714c8ae320d364b7f1f694366e8bf7cac6bf69b2d47b44df91df0fa3044515709dee74139204cd0cf068a399f631af3bcf673d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AoApp::Application.config.session_store :cookie_store, key: '_ao_app_session'
